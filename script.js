9c6a88502070033a66224f8e6fe50a0196659413
nash-ds <129283129+nash-ds@users.noreply.github.com>
2023-10-16 23:12:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+function showDescs(disaster) {
+    const descs = {
+        one: ['description 1', 'description 2', 'description 3'],
+        two: ['description 1', 'description 2', 'description 3'],
+        three: ['description 1', 'description 2', 'description 3'],
+        four: ['description 1', 'description 2', 'description 3'],
+        five: ['description 1', 'description 2', 'description 3']
+    };
 
+    const descsList = document.getElementById('descs-list');
+    const disasterTitle = document.getElementById('disaster-title');
+    
+    descsList.innerHTML = '';
+
+    if (descs[disaster]) {
+        descs[disaster].forEach(desc => {
+            const listItem = document.createElement('li');
+            listItem.textContent = desc;
+            descsList.appendChild(listItem);
+        });
+        disasterTitle.textContent = `description in ${disaster.charAt(0).toUpperCase() + disaster.slice(1)}`;
+    } else {
+        disasterTitle.textContent = 'No desc available.';
+    }
+}
