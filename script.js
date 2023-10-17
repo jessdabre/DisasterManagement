f212c204a53b7f91a1fcb220964da6e1d700f5b5
jessdabre <122386691+jessdabre@users.noreply.github.com>
2023-10-17 11:02:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,47 @@
 function showDescs(disaster) {
     const descs = {
-        Disasters: ['Emergency Alerts', 'Drought', 'Earthquake','Floods', 'Tsunamis'],
-        Make: ['description 1', 'description 2', 'description 3'],
-        Get: ['description 1', 'description 2', 'description 3'],
-        four: ['description 1', 'description 2', 'description 3'],
-        five: ['description 1', 'description 2', 'description 3']
+        one: [
+            { text: 'Emergency Alerts', link: 'description1.html' },
+            { text: 'Make a plan', link: 'description2.html' },
+            { text: 'Get involved', link: 'description3.html' }
+        ],
+        two: [
+            { text: 'Description 1', link: 'description1.html' },
+            { text: 'Description 2', link: 'description2.html' },
+            { text: 'Description 3', link: 'description3.html' }
+        ],
+        three: [
+            { text: 'Description 1', link: 'description1.html' },
+            { text: 'Description 2', link: 'description2.html' },
+            { text: 'Description 3', link: 'description3.html' }
+        ],
+        four: [
+            { text: 'Description 1', link: 'description1.html' },
+            { text: 'Description 2', link: 'description2.html' },
+            { text: 'Description 3', link: 'description3.html' }
+        ],
+        five: [
+            { text: 'Description 1', link: 'description1.html' },
+            { text: 'Description 2', link: 'description2.html' },
+            { text: 'Description 3', link: 'description3.html' }
+        ]
     };
+
     const descsList = document.getElementById('descs-list');
     const disasterTitle = document.getElementById('disaster-title');
+
     descsList.innerHTML = '';
+
     if (descs[disaster]) {
-        descs[disaster].forEach(desc => {
+        descs[disaster].forEach(descItem => {
             const listItem = document.createElement('li');
-            listItem.textContent = desc;
+            const link = document.createElement('a');
+            link.href = descItem.link;
+            link.textContent = descItem.text;
+            listItem.appendChild(link);
             descsList.appendChild(listItem);
         });
-        disasterTitle.textContent = `description in ${disaster.charAt(0).toUpperCase() + disaster.slice(1)}`;
+        disasterTitle.textContent = `Description in ${disaster.charAt(0).toUpperCase() + disaster.slice(1)}`;
     } else {
         disasterTitle.textContent = 'No desc available.';
     }
