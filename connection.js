364a04284877a1cc890657e4bfea1ae72cf29cdc
jessdabre <122386691+jessdabre@users.noreply.github.com>
2024-01-21 19:08:55 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,20 +1,59 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyDeOqeLcqGVfjAPFfvbWjKhbphwxHDR5Mo",
-  authDomain: "disastermanagement-402118.firebaseapp.com",
-  databaseURL: "https://disastermanagement-402118-default-rtdb.firebaseio.com",
-  projectId: "disastermanagement-402118",
-  storageBucket: "disastermanagement-402118.appspot.com",
-  messagingSenderId: "646530051011",
-  appId: "1:646530051011:web:98d15e0321a6ec9400ab76",
-  measurementId: "G-D42ZG3BYN2"
+  apiKey: "AIzaSyD6390Fa5DmjQBUSZ_lhddIi4ijUyySHP0",
+  authDomain: "login-76465.firebaseapp.com",
+  databaseURL: "https://login-76465-default-rtdb.firebaseio.com",
+  projectId: "login-76465",
+  storageBucket: "login-76465.appspot.com",
+  messagingSenderId: "433659304050",
+  appId: "1:433659304050:web:4f5267423ad66c93bd8aeb",
+  measurementId: "G-H5P1EMSMEY"
 };
 
-//initialize
+
+// initialize firebase
 firebase.initializeApp(firebaseConfig);
-var LoginInfoDB = firebase.database().ref('LoginInfo');
-document.getElementbyId("loginform").addEventListener("submit", subitForm);
 
+// reference your database
+var contactFormDB = firebase.database().ref("contactForm");
 
-function subitFrom(e){
+document.getElementById("contactForm").addEventListener("submit", submitForm);
+
+function submitForm(e) {
   e.preventDefault();
+
+  var name = getElementVal("name");
+  var phn1 = getElementVal("phn1");
+  var phn2 = getElementVal("phn2");
+  var bg = getElementVal("bg");
+  var condition = getElementVal("condition");
+  var message = getElementVal("message");
+  saveMessages(name,phn1,phn2,bg,condition,message);
+
+  //   enable alert
+  document.querySelector(".alert").style.display = "block";
+
+  //   remove the alert
+  setTimeout(() => {
+    document.querySelector(".alert").style.display = "none";
+  }, 3000);
+
+  //   reset the form
+  document.getElementById("contactForm").reset();
 }
+
+const saveMessages = (name,phn1,phn2,bg,condition,message) => {
+  var newContactForm = contactFormDB.push();
+
+  newContactForm.set({
+        Name : name,
+        Phonenumber1 : phn1,
+        Phonenumber2 : phn2,
+        BloodGroup : bg,
+        MedicalCondition : condition,
+        Message : message,
+  });
+};
+
+const getElementVal = (id) => {
+  return document.getElementById(id).value;
+};
